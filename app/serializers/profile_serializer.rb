099b542c39ab45a444767818336082cc883d7822
owenayora <ayoraowen@gmail.com>
2023-05-17 13:49:32 +0300

    Completed all deliverables

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,4 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :username, :email, :bio, :avatar_url
-  belongs_to :author
+  # belongs_to :author #it appears we didnt need this here. Why, i dont know exactly it wants us to go back to controller n use include ['profile']
 end
